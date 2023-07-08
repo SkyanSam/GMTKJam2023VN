3fb3fde0beec80e8a6d2f9635da10ab6f03fb553
LuneEve <soonmin2210@gmail.com>
2023-07-08 18:57:45 -0400

    Update SwapUI.gd

--- SwapUI/SwapUI.gd
+++ SwapUI/SwapUI.gd
@@ -9,8 +9,8 @@ export var characterNames : PoolStringArray
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for currentCharacter in characterNames:
-		if(!Global.story_manager.story.variablesState[currentCharacter]):
-			
+		if Global.story_manager.story.variablesState[currentCharacter]:
+			getNode(currentCharacter)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
