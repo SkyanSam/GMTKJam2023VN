1d9dd78884cbd720dbbd053df76f95dec025680f
LuneEve <soonmin2210@gmail.com>
2023-07-08 19:22:53 -0400

    Update SwapUI.gd

--- SwapUI/SwapUI.gd
+++ SwapUI/SwapUI.gd
@@ -9,10 +9,15 @@ export var characterNames : PoolStringArray
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for currentCharacter in characterNames:
-		if Global.story_manager.story.variablesState[currentCharacter]:
+		if !Global.story_manager.story.variablesState[currentCharacter]:
 			get_node(currentCharacter).modulate = Color(86,86,86)
 		else:
 			get_node(currentCharacter).modulate = Color(0,0,0)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_TextureButton_pressed():
+	if !Global.story_manager.story.variablesState["canUseTim"]:
+		Global.story_manager.story.variablesState["currentNPC"] = "Tim"
