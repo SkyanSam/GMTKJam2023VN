31a5c4b0822372ae9fb0218da12899129d0c43dd
Kartavya Gaur <68074579+CrazyRobotCoder@users.noreply.github.com>
2023-07-07 21:21:54 -0400

    got buttons on main menu, one we get pages, link them to stuff

--- Dialogue/Triggers/DialogueInstantTrigger.gd
+++
@@ -1,4 +0,0 @@
-extends "res://Dialogue/Triggers/DialogueTrigger.gd"
-
-func _ready():
-	trigger_story()
--- Dialogue/Triggers/DialogueTrigger.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-export var label = ""
-export var path = ""
-
-func trigger_story():
-	Global.story_manager.Start(path, label)
