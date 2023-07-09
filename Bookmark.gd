5bf0f4f35448538d48afe1ed775e702e34c176b8
Kartavya Gaur <68074579+CrazyRobotCoder@users.noreply.github.com>
2023-07-09 08:22:10 -0400

    stuff that is suppposed  to work

--- Bookmark.gd
+++ Bookmark.gd
@@ -4,7 +4,7 @@ extends TextureRect
 bool isOpen = false
 
 func _ready():
-	
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ SwapUI/NPC_Scripts/Tim.gd
@@ -0,0 +1,18 @@
+extends TextureRect
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Global.story_manager.story.variablesState["canUseBarkeeper"] == true:
+		$Barkeeper.show()
+	else: 
+		$Barkeeper.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
