9373b55d779431f4253dc69c2d612086283ed39c
Kartavya Gaur <68074579+CrazyRobotCoder@users.noreply.github.com>
2023-07-08 20:27:20 -0400

    sus meter?!?!?!?!??!?!

--- Global.gd
+++ Global.gd
@@ -1,18 +1,15 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 const StoryManager = preload("res://Dialogue/StoryManager/StoryManager.cs")
 
-onready var story_manager : StoryManager
+onready var story_manager : StoryManager = get_node("/root/Game/StoryManager")
 
 onready var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var temp_story_manager = get_node("/root/Node2D/StoryManager")
+	var temp_story_manager = get_node("/root/Game/StoryManager")
 	story_manager = temp_story_manager
 	rng.randomize()
 	print("story_manager : " + str(story_manager))
